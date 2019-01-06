4f01208d65861bfe84aec949cf65d8c504d5e875
Zachary Tong <tongcheefei@gmail.com>
2019-01-06 23:12:14 +0800

    Migrate androidx

--- app/src/main/java/com/delivery/delivery/HomeActivity.kt
+++ app/src/main/java/com/delivery/delivery/HomeActivity.kt
@@ -1,6 +1,6 @@
 package com.delivery.delivery
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HomeActivity : AppCompatActivity() {
